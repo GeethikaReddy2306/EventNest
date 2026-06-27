6dac0c9a05a1594df2ecf221539837a60ed91ae6
geethika <geethikareddy051@gmail.com>
2026-06-27 19:18:26 +0530

    feat: Update axios base URL to include '/api' for correct API endpoint

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://event-nest-seven.vercel.app',
+    baseURL: 'https://event-nest-seven.vercel.app/api',
 });
 
 api.interceptors.request.use((config) => {
